c982d38221bf80b5470edf5a447bdd305fd64ce9
Brandon Yao <ahoynodnarb@gmail.com>
2020-05-31 04:48:42 -0700

    Added preferencebundle

---
+++ fivepercentalertprefs/PTDRootListController.m
@@ -0,0 +1,13 @@
+#include "PTDRootListController.h"
+
+@implementation PTDRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
